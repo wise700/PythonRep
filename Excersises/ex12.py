a9c845acffa5b2739bb9401bfd92611d28782df8
Hrushikesh Mysore Srinivas <Hrushikesh_M@ad.infosys.com>
2017-05-21 16:21:03 +0530

    Changed ex12

--- Excersises/ex12.py
+++ Excersises/ex12.py
@@ -3,3 +3,4 @@ Height=int(input("How tall are you?"))
 weight=int(input("How weight are you?"))
 
 print("so u are %d years old, %d height and %d weight"%(age,Height,weight))
+print("Git change torso")
